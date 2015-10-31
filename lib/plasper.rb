54a68090f34c72a781eacf1be9b153999ff40417
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-10-31 03:44:58 +0300

    fix #2, counting letter weights

--- lib/plasper.rb
+++ lib/plasper.rb
@@ -1,13 +1,18 @@
 module Plasper
   class Plasper
-    attr_reader :length_weight
+    attr_reader :length_weight, :letter_weight
 
     def initialize
       @length_weight = Hash.new(0)
+      @letter_weight = Hash.new(0)
     end
 
+    # @param [String] word
     def add_word(word)
       @length_weight[word.length] += 1
+      word.each_char { |letter| @letter_weight[letter] += 1 }
     end
+
+    private
   end
 end
--- spec/plasper_spec.rb
+++ spec/plasper_spec.rb
@@ -14,11 +14,19 @@ describe Plasper::Plasper do
       plasper.add_word 'other'
       expect(plasper.length_weight[5]).to eq(2)
     end
-    
-    it 'counts letter weight for each letter in word'
+
+    it 'counts letter weight for each letter in word' do
+      plasper.add_word 'ага'
+      expect(plasper.letter_weight).to eq('а' => 2, 'г' => 1)
+    end
+
     it 'adds first_letter weight'
     it 'increases first_letter weight'
     it 'adds next letter weight'
     it 'increases next letter weight'
+  end
+
+  describe '#add_sentence' do
+
   end
 end
